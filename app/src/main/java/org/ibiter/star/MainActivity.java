7d79e13186d4d0c9de6e0b2c4960d9f0690cc939
AntiverX <Antiver@foxmail.com>
2016-09-23 17:46:30 +0800

    2016-9-23
    实现fragment

--- app/src/main/java/org/ibiter/star/MainActivity.java
+++ app/src/main/java/org/ibiter/star/MainActivity.java
@@ -1,41 +1,41 @@
 package org.ibiter.star;
 
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
-import com.baidu.location.Poi;
-import org.ibiter.star.LocationService;
-
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-/***
- * 单点定位示例，用来展示基本的定位结果，配置在LocationService.java中
- * 默认配置也可以在LocationService中修改
- * 默认配置的内容自于开发者论坛中对开发者长期提出的疑问内容
- *
- * @author baidu
- *
+/**
+ * Created by Coder-pig on 2015/8/28 0028.
  */
-public class MainActivity extends AppCompatActivity {
-    private LocationService locationService;
-    private TextView LocationResult;
-    private Button startLocation;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //UI Object
+    private TextView txt_topbar;
+    private TextView txt_channel;
+    private TextView txt_message;
+    private TextView txt_better;
+    private TextView txt_setting;
+    private FrameLayout ly_content;
+
+    //Fragment Object
+    private MyFragment fg1,fg2,fg3,fg4;
+    private LocationFragment lf;
+    private FragmentManager fManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        // -----------demo view config ------------
-        setContentView(R.layout.location);
+        setContentView(R.layout.activity_main);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
@@ -49,162 +49,89 @@ public class MainActivity extends AppCompatActivity {
             window.setNavigationBarColor(Color.TRANSPARENT);
         }
 
-        LocationResult = (TextView) findViewById(R.id.textView1);
-        LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
-        startLocation = (Button) findViewById(R.id.addfence);
 
+        fManager = getFragmentManager();
+        bindViews();
+        txt_channel.performClick();   //模拟一次点击，既进去后选择第一项
     }
 
-    /**
-     * 显示请求字符串
-     *
-     * @param str
-     */
-    public void logMsg(String str) {
-        try {
-            if (LocationResult != null)
-                LocationResult.setText(str);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    //UI组件初始化与事件绑定
+    private void bindViews() {
+        txt_channel = (TextView) findViewById(R.id.txt_channel);
+        txt_message = (TextView) findViewById(R.id.txt_message);
+        txt_better = (TextView) findViewById(R.id.txt_better);
+        txt_setting = (TextView) findViewById(R.id.txt_setting);
+        ly_content = (FrameLayout) findViewById(R.id.ly_content);
+
+        txt_channel.setOnClickListener(this);
+        txt_message.setOnClickListener(this);
+        txt_better.setOnClickListener(this);
+        txt_setting.setOnClickListener(this);
+    }
+
+    //重置所有文本的选中状态
+    private void setSelected(){
+        txt_channel.setSelected(false);
+        txt_message.setSelected(false);
+        txt_better.setSelected(false);
+        txt_setting.setSelected(false);
+    }
+
+    //隐藏所有Fragment
+    private void hideAllFragment(FragmentTransaction fragmentTransaction){
+        if(fg1 != null)fragmentTransaction.hide(fg1);
+        if(fg2 != null)fragmentTransaction.hide(fg2);
+        if(fg3 != null)fragmentTransaction.hide(fg3);
+        if(lf != null)fragmentTransaction.hide(lf);
     }
 
 
-    /***
-     * Stop location service
-     */
     @Override
-    protected void onStop() {
-        // TODO Auto-generated method stub
-        locationService.unregisterListener(mListener); //注销掉监听
-        locationService.stop(); //停止定位服务
-        super.onStop();
-    }
-
-    @Override
-    protected void onStart() {
-        // TODO Auto-generated method stub
-        super.onStart();
-        // -----------location config ------------
-        locationService = ((LocationApplication) getApplication()).locationService;
-        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
-        locationService.registerListener(mListener);
-        //注册监听
-        int type = getIntent().getIntExtra("from", 0);
-        if (type == 0) {
-            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
-        } else if (type == 1) {
-            locationService.setLocationOption(locationService.getOption());
+    public void onClick(View v) {
+        FragmentTransaction fTransaction = fManager.beginTransaction();
+        hideAllFragment(fTransaction);
+        switch (v.getId()){
+            case R.id.txt_channel:
+                setSelected();
+                txt_channel.setSelected(true);
+                if(fg1 == null){
+                    fg1 = new MyFragment("第一个Fragment");
+                    fTransaction.add(R.id.ly_content,fg1);
+                }else{
+                    fTransaction.show(fg1);
+                }
+                break;
+            case R.id.txt_message:
+                setSelected();
+                txt_message.setSelected(true);
+                if(fg2 == null){
+                    fg2 = new MyFragment("第二个Fragment");
+                    fTransaction.add(R.id.ly_content,fg2);
+                }else{
+                    fTransaction.show(fg2);
+                }
+                break;
+            case R.id.txt_better:
+                setSelected();
+                txt_better.setSelected(true);
+                if(fg3 == null){
+                    fg3 = new MyFragment("第三个Fragment");
+                    fTransaction.add(R.id.ly_content,fg3);
+                }else{
+                    fTransaction.show(fg3);
+                }
+                break;
+            case R.id.txt_setting:
+                setSelected();
+                txt_setting.setSelected(true);
+                if(lf == null){
+                    lf = new LocationFragment();
+                    fTransaction.add(R.id.ly_content,lf);
+                }else{
+                    fTransaction.show(lf);
+                }
+                break;
         }
-        startLocation.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
-                    locationService.start();// 定位SDK
-                    // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
-                    startLocation.setText(getString(R.string.stoplocation));
-                } else {
-                    locationService.stop();
-                    startLocation.setText(getString(R.string.startlocation));
-                }
-            }
-        });
+        fTransaction.commit();
     }
-
-
-    /*****
-     * @see copy funtion to you project
-     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
-     *
-     */
-    private BDLocationListener mListener = new BDLocationListener() {
-
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            // TODO Auto-generated method stub
-            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
-                StringBuffer sb = new StringBuffer(256);
-                sb.append("time : ");
-                /**
-                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
-                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
-                 */
-                sb.append(location.getTime());
-                sb.append("\nlocType : ");// 定位类型
-                sb.append(location.getLocType());
-                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
-                sb.append(location.getLocTypeDescription());
-                sb.append("\nlatitude : ");// 纬度
-                sb.append(location.getLatitude());
-                sb.append("\nlontitude : ");// 经度
-                sb.append(location.getLongitude());
-                sb.append("\nradius : ");// 半径
-                sb.append(location.getRadius());
-                sb.append("\nCountryCode : ");// 国家码
-                sb.append(location.getCountryCode());
-                sb.append("\nCountry : ");// 国家名称
-                sb.append(location.getCountry());
-                sb.append("\ncitycode : ");// 城市编码
-                sb.append(location.getCityCode());
-                sb.append("\ncity : ");// 城市
-                sb.append(location.getCity());
-                sb.append("\nDistrict : ");// 区
-                sb.append(location.getDistrict());
-                sb.append("\nStreet : ");// 街道
-                sb.append(location.getStreet());
-                sb.append("\naddr : ");// 地址信息
-                sb.append(location.getAddrStr());
-                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
-                sb.append(location.getUserIndoorState());
-                sb.append("\nDirection(not all devices have value): ");
-                sb.append(location.getDirection());// 方向
-                sb.append("\nlocationdescribe: ");
-                sb.append(location.getLocationDescribe());// 位置语义化信息
-                sb.append("\nPoi: ");// POI信息
-                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
-                    for (int i = 0; i < location.getPoiList().size(); i++) {
-                        Poi poi = (Poi) location.getPoiList().get(i);
-                        sb.append(poi.getName() + ";");
-                    }
-                }
-                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-                    sb.append("\nspeed : ");
-                    sb.append(location.getSpeed());// 速度 单位：km/h
-                    sb.append("\nsatellite : ");
-                    sb.append(location.getSatelliteNumber());// 卫星数目
-                    sb.append("\nheight : ");
-                    sb.append(location.getAltitude());// 海拔高度 单位：米
-                    sb.append("\ngps status : ");
-                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
-                    sb.append("\ndescribe : ");
-                    sb.append("gps定位成功");
-                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-                    // 运营商信息
-                    if (location.hasAltitude()) {// *****如果有海拔高度*****
-                        sb.append("\nheight : ");
-                        sb.append(location.getAltitude());// 单位：米
-                    }
-                    sb.append("\noperationers : ");// 运营商信息
-                    sb.append(location.getOperators());
-                    sb.append("\ndescribe : ");
-                    sb.append("网络定位成功");
-                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                    sb.append("\ndescribe : ");
-                    sb.append("离线定位成功，离线定位结果也是有效的");
-                } else if (location.getLocType() == BDLocation.TypeServerError) {
-                    sb.append("\ndescribe : ");
-                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
-                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-                }
-                logMsg(sb.toString());
-            }
-        }
-
-    };
 }
---
+++ app/src/main/java/org/ibiter/star/MyFragment.java
@@ -0,0 +1,35 @@
+package org.ibiter.star;
+
+/**
+ * Created by Antiver M on 2016/9/23.
+ */
+
+import android.R.integer;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author linuxiao
+ * Email: linuxiao@gmail.com
+ * @date 2014年10月9日 下午5:39:18
+ *
+ */
+public class MyFragment extends Fragment {
+
+    private String content;
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
